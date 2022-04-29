46ee40fb60d3ba1b2d744ba4510d3145480a5675
Python The Hack <lrulvoni@gmail.com>
2022-04-29 17:25:23 +0200

    update database seeder

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,36 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Listing {
-    public static function all() {
-        return [
-
-            [
-                'id' => 1,
-                'title' => 'listing one',
-                'description' => 'lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                 Ratione iusto voluptates sequi, dignissimos praesentium velit quae repellat ab tenetur,
-                 illum saepe suscipit minus distinctio? Itaque sit sequi consequuntur cum possimus! '
-            ],
-            [
-                'id' => 2,
-                'title' => 'listing Two',
-                'description' => 'lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                 Ratione iusto voluptates sequi, dignissimos praesentium velit quae repellat ab tenetur,
-                 illum saepe suscipit minus distinctio? Itaque sit sequi consequuntur cum possimus! '
-            ]
-
-            ];
-    }
-
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
