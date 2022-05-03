73c33da687d89b776397679bfa7d37a24e26fc61
Python The Hack <lrulvoni@gmail.com>
2022-05-03 17:08:23 +0200

    update back card to dashboard

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Models\Listing;
 //All listings
 Route::get('/', function () {
     return view('listings', [
-        'heading' => 'Latest Listings',
+        'heading' => 'Ultimi posts pubblicati',
 
         'listings' => Listing::all()
     ]);
